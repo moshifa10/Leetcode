5ea6af0a799fd1da3a3fc7e469daa05b5d9c139a
Njabulo Moshifa <njmosjhb025@wstudent.wethinkcode.co.za>
2026-02-16 13:48:16 +0200

    working but failing somewhere

--- VigenèreCipherHelper.py
+++ VigenèreCipherHelper.py
@@ -6,12 +6,13 @@ class VigenereCipher(object):
         self.current_pos = 0
     
     def encode(self, text):
+        self.current_pos = 0
         # Go to key using current pos, Get the index from alpha and go back to the current pos of the letter and move it forward
         word: list = []
         for each_word in text:
             # print(each_word)
             # Now get the current index from alpha
-            if each_word.isalpha():
+            if each_word.isalpha() and each_word in self.alphabet:
                 current_index = self.alphabet.index(self.key[self.current_pos])
                 
 
@@ -21,7 +22,7 @@ class VigenereCipher(object):
                 if total > 25 :
                     shift = abs((total - (len(self.alphabet)-1)) - 1)   
                     word.append(self.alphabet[shift])
-
+   
                 else:word.append(self.alphabet[total])
 
                 self.current_pos += 1
@@ -33,13 +34,42 @@ class VigenereCipher(object):
         # print(word)
 
     def decode(self, text):
-        pass
+        self.current_pos = 0
+        word: list = []
+
+        for each_word in text:
+            if each_word.isalpha() and each_word in self.alphabet:
+                current_index_from_text = self.alphabet.index(each_word)
+                current_index_from_alphabet = self.alphabet.index(self.key[self.current_pos])
+
+                # print(f"key: {current_index_from_alphabet} \ttext:{current_index_from_text}" )
+                # break
+                total = (current_index_from_text - current_index_from_alphabet)
+                # print(total)
+
+                word.append(self.alphabet[total])
+
+                self.current_pos += 1
+
+            else: word.append(each_word)
+
+        return "".join(word)
 
 
 s = VigenereCipher(key="password",alphabet="abcdefghijklmnopqrstuvwxyz" )
-abc = "abcdefghijklmnopqrstuvwxyz"
-key = "password"
+# abc = "abcdefghijklmnopqrstuvwxyz"
+# key = "password"
 
 
 # ovnlqbpvt hznzeuz
-s.encode("codewars") # rovwsoiv
+# print(s.decode("rovwsoiv") )# rovwsoiv #codewars
+
+
+
+print(s.encode('codewars'))#, 'rovwsoiv'
+print(s.decode('rovwsoiv'))#, 'codewars'
+print(s.encode('waffles'))#, 'laxxhsj'
+print(s.decode('laxxhsj'))#, 'waffles'
+print(s.encode('CODEWARS'))#, 'CODEWARS'
+print(s.decode('CODEWARS'))#, 'CODEWARS'
+print(s.encode(("it's a shift cipher!"))) # , "xt'k o vwixl qzswej!"
