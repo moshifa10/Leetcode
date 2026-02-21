0921d5fcc3c58f789170cc96f9d87fe0cbffeeda
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-02-22 00:11:50 +0200

    fixed and the way I see it should pass but nah its not

--- MTVCribs.py
+++ MTVCribs.py
@@ -38,29 +38,79 @@
 # print("   /\\   \n  /  \\  \n /    \\ \n/______\\\n|      |\n|      |\n|______|")
 # print(" /\\ \n/__\\\n|__|")
 
+# def my_crib(n):
+    
+#     space = n
+#     d = n+n
+#     ticks = n+1
+#     inside_space = 0
+
+#     down_ticks = n
+    
+#     for i in range(1, n+1):
+#         print(f"{" "*space}{"/"}{" "*inside_space}{"\\"}")
+#         if space == 1:
+#             # print(f"{" "* space}{"/"}{"-"*inside_space}{"\\"}")
+#             break
+#         space-=1
+#         inside_space +=2
+    
+#     print(f"{" "* space}{"/"}{"-"*inside_space}{"\\"}")
+
+#     for i in range(1,n+1):
+#         if i == n:
+#             print(f"{" "*space}{"|"}{"_"*inside_space}{"|"}")
+#         else:
+#             print(f"{" "*space}{"|"}{" "*inside_space}{"|"}")
+# def my_crib(n):
+    
+#     space = n+1
+#     d = n+n
+#     ticks = n+1
+#     inside_space = 0
+
+#     down_ticks = n
+    
+#     for i in range(1, n+1):
+#         print(f"{" "*space}{"/"}{" "*inside_space}{"\\"}")
+#         if space == 1:
+#             break
+#         space-=1
+#         inside_space +=2
+    
+#     print(f"{" "* space}{"/"}{"_"*inside_space}{"\\"}")
+
+#     for i in range(1,n+1):
+#         if i == n:
+#             print(f"{" "*space}{"|"}{"_"*inside_space}{"|"}")
+#         else:
+#             print(f"{" "*space}{"|"}{" "*inside_space}{"|"}")
+# my_crib(3)
+
 def my_crib(n):
     
     space = n+1
     d = n+n
     ticks = n+1
     inside_space = 0
-
+    
     down_ticks = n
+    house = ""
     
     for i in range(1, n+1):
-        print(f"{" "*space}{"/"}{" "*inside_space}{"\\"}")
-        if space == 2:
-            print(f"{" "* space}{"/"}{"-"*inside_space}{"\\"}")
+        house +=f'{" "*space}/{" "*inside_space}\\ \n'
+        if space == 1:
             break
         space-=1
         inside_space +=2
     
-    # print(f"{" "* space}{"/"}{"-"*inside_space}{"\\"}")
+    house +=f'{" "* space}/{"_"*inside_space}\\ \n'
 
     for i in range(1,n+1):
         if i == n:
-            print(f"{" "*space}{"|"}{"_"*inside_space}{"|"}")
+            house += f'{" "*space}|{"_"*inside_space}| \n'
         else:
-            print(f"{" "*space}{"|"}{" "*inside_space}{"|"}")
-
-my_crib(4)
+            house += f'{" "*space}|{" "*inside_space}| \n'
+            
+    return house
+print(my_crib(3))
