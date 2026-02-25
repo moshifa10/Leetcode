ffe83c4c3dfa4dca9475760eac46e44333c145cd
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-02-25 21:50:19 +0200

    Done

--- build_palindrome.py
+++ build_palindrome.py
@@ -1,10 +1,38 @@
 
 
-def build_palindrome(st):
-    l = st
+def build_palindrome(st: str):
+    l = st[-1]
+
+    idx = st.index(l)
+
+
+    # if st+ (st[: idx][::-1]) == (st+ (st[: idx][::-1]))[::-1]:
+    #     return st+ (st[: idx][::-1])
+    
+    
+    s = idx
+    f = s
+    current = st+ (st[: f][::-1])
+    modified = current[::-1]
+
+    while current != modified:
+        current = st+ (st[: f][::-1])
+        modified = current[::-1]
+
+        f = st.index(l,s)
+        s += 1
+    print(current)
+    return current
 
 
 
-build_palindrome("abcdc")
+
+# print(build_palindrome("abcdc"))
 # test.assert_equals(build_palindrome("abcdc"),"abcdcba")
 # test.assert_equals(build_palindrome("ababab"),"abababa")
+
+print(build_palindrome("ccgaaegeg"))
+
+
+
+# ccgaaegegcc ccgaaegeg -> ccgaaegegeaagcc
