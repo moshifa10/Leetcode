0f4e9ac196db50ef9df1352bd0b120c00db32c44
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-02-25 14:17:58 +0200

    added palindrome

---
+++ build_palindrome.py
@@ -0,0 +1,10 @@
+
+
+def build_palindrome(st):
+    l = st
+
+
+
+build_palindrome("abcdc")
+# test.assert_equals(build_palindrome("abcdc"),"abcdcba")
+# test.assert_equals(build_palindrome("ababab"),"abababa")
