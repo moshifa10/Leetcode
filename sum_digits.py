86683538d114a61dc4daf4132a17c8d28245abbb
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-02-26 14:19:28 +0200

    added sum digits and done

---
+++ sum_digits.py
@@ -0,0 +1,17 @@
+
+
+'''
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+'''
+
+
+def digital_root(n):
+    while len(str(n)) != 1:
+        s = list(map(int,list(str(n))))
+        n = sum(s)
+
+    print(n)
+
+digital_root(942)
