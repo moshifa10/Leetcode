36ab11d31910dd537e89c54672d5e2e702c3004f
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-03-06 23:14:01 +0200

    commited

---
+++ ThreeaddedCharacters.py
@@ -0,0 +1,25 @@
+
+
+
+def added_char(s1, s2):  
+    
+    for i in s2:
+        if i not in s1:
+            return i
+        
+        elif s2.count(i) != s1.count(i):
+            return i
+
+
+
+
+string1 = "aabbcc"
+string2 = "aacccbbcc"
+# // => 'c'
+
+
+'''test.assert_equals(added_char("hello","checlclo"),'c')
+test.assert_equals(added_char("aabbcc","aacccbbcc"),'c')
+test.assert_equals(added_char("abcde","2db2a2ec"),'2')'''
+
+print(added_char("hello","checlclo"))
